f98e963785bc6315385c680b86e391166fec2182
Chupacabriks <mufunbetsuna@gmail.com>
2025-06-03 16:33:50 +0300

    add: modal window,  function toggle for modal

--- js/main.js
+++ js/main.js
@@ -107,3 +107,34 @@ const swiperBlog = new Swiper('.blog-slider', {
     },
   }
 });
+
+
+/*const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+const modalClose = document.querySelector('.modal-close')
+modalToggle.forEach((element) =>
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.add('modal-is-open')
+  }));
+modalClose.addEventListener('click', (event) => {
+  event.preventDefault;
+  modal.classList.remove('modal-is-open')
+}
+)*/
+const modal = document.querySelector('.modal');
+const modalDialog = document.querySelector('.modal-dialog');
+
+document.addEventListener('click', (event) => {
+  if (event.target.dataset.toggle == 'modal' ||
+    event.target.parentNode.dataset.toggle == 'modal' || (!event.composedPath().includes(modalDialog) && modal.classList.contains('modal-is-open'))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle('modal-is-open')
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.key == 'Escape' && modal.classList.contains('modal-is-open')) {
+    modal.classList.toggle('modal-is-open');
+  }
+})
